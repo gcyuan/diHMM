c86e65f864bb187b0482e32ad18713ff0a7bac6f
eugeniomarco <eugenio.marco@gmail.com>
2015-07-28 23:41:01 -0400

    Added support for mm10 and hg38 genomes

--- main/getChrList.m
+++ main/getChrList.m
@@ -5,13 +5,13 @@ if ~exist('genome', 'var')
 end
 
 switch genome
-    case {'hg19', 'hg18', 'hg17'}
+    case {'hg38', 'hg19', 'hg18', 'hg17'}
         chrList = cell(23,1);
         for index = 1:length(chrList)-1
             chrList{index} = ['chr' num2str(index)];
         end
         chrList{23} = 'chrX';
-    case 'mm9'
+    case {'mm10', 'mm9'}
         chrList = cell(20,1);
         for index = 1:length(chrList)-1
             chrList{index} = ['chr' num2str(index)];
